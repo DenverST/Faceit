1d7547450f3db985137b877fc0f3f5e579f7163d
Denver Stove <dtstove@gmail.com>
2018-10-23 22:01:25 +1100

    Built FacialExpression struct

--- Faceit/ViewController.swift
+++ Faceit/ViewController.swift
@@ -9,6 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var faceView: FaceView! {
+        didSet {
+            updateUI()
+        }
+    }
+    
+    var expression = FacialExpression(eyes: .closed, mouth: .frown) {
+        didSet {
+            updateUI()
+        }
+    }
+    
+    private func updateUI() {
+        switch expression.eyes {
+        case .open:
+            faceView?.eyeOpen = true
+        case .closed:
+            faceView?.eyeOpen = false
+        case .squinting:
+            faceView?.eyeOpen = false
+        }
+        faceView.mouthCurvature = mouthCurvatures[expression.mouth] ?? 0.0
+    }
+    
+    private let mouthCurvatures = [FacialExpression.Mouth.grin:0.5,.frown:-1.0,.smile:1.0,.neutral:1.0,.smirk:-0.5]
 }
 
