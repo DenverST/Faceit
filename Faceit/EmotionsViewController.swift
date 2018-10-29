6ab586536a4d02df8de9d709caf5a75f6a8bab28
Denver Stove <dtstove@gmail.com>
2018-10-30 09:58:14 +1100

    Creating emotions ViewController

---
+++ Faceit/EmotionsViewController.swift
@@ -0,0 +1,23 @@
+//
+//  EmotionsViewController.swift
+//  Faceit
+//
+//  Created by Denver Stove on 30/10/18.
+//  Copyright © 2018 Denver Stove. All rights reserved.
+//
+
+import UIKit
+
+class EmotionsViewController: UIViewController {
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
